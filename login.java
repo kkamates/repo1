4089c3a80c2ed128532cfc59bc2bf1d826bae356
kkamates <kantakamates@gmail.com>
2019-01-31 19:48:23 +0530

    login.java
     login.java

--- login.java
+++ login.java
@@ -3,3 +3,6 @@
  *
  * but -am loging .java didnt work
  * it worked "git commit -am "trying once more" "
+ *
+ * but($ git commit -am login.java "msg" ) didnt work.
+ * because  if we are usinmg -am option we can commit without specifing the file name
